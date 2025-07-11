d254307cc9965297cc98f11a1537d1ef30d819b0
ssehomedecor <ssehomedecor@gmail.com>
2025-07-11 15:58:24 +0530

    second ommit

---
+++ src/pages/Products.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Products() {
+  return (
+    <div>
+      <h1>Our Products</h1>
+      <ul>
+        <li>Mesh Doors</li>
+        <li>Mosquito Nets</li>
+        <li>Window Blinds</li>
+        <li>Custom Home Decor Solutions</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Products;
